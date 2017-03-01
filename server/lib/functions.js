830103f724f78b21f7b7dfdc42e3ec0b4d7c712b
Ken <tkg214@gmail.com>
2017-03-01 05:19:28 +0000

    Nothing to add

---
+++ server/lib/functions.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const settings = require('./knexsettings.json'); // TODO CHANGE THIS TO CONFIG DB
+const knex = require('knex')(settings);
+
+// Defines helper functions for saving and getting tweets, using the database `db`
+module.exports = {
+
+  // TODO MAKE SURE CALLBACKS ETC ARE CHANGED BELOW
+  getAllPosts: (done) => {
+    knex.select().from('posts').then(done);
+  },
+  getAllPostsForUser: (user, done) => {
+    knex.select().from('posts').where({
+      user_id: user
+    }).then(done);
+  },
+  createPost: (data, done) => {
+    knex.insert({
+      // ENTER
+    }).into('posts').then(done);
+  },
+  updatePost: (     ) => {
+    knex('posts').where({
+      // ENTER
+    }).update({
+      // ENTER
+    }).then(done);
+  },
+  deletePost: (    ) => {
+    knex('posts').where({
+      // ENTER
+    }).del().then(done);
+  }
+};
