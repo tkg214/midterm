8fab91367402ccb8479f7bba8352c2fea149e9db
Hans Kurniadi <hans.d.k@live.com>
2017-03-03 02:37:35 +0000

    implement login ajax request

--- public/scripts/user-login.js
+++ public/scripts/user-login.js
@@ -1,23 +1,17 @@
 $(document).ready(function() {
-  //Selectors need to be changed
   $('#login-form').on('submit', function(event) {
     event.preventDefault();
     const $this = $(this);
-    // console.log($this);
-    const username = $(this).find('.username').val();
-    const password = $(this).find('.password').val();
-    // console.log('username', username);
-    // console.log('password', password);
-    // alert('clicked');
-    // $.ajax({
-    //   method: 'POST',
-    //   url: '/login',
-    //   data: {
-    //     username: username,
-    //     password: password
-    //   }
-    // }).then(() => {
-    //   //createPost(results);
-    // });
+    const handle = $(this).find('.handle').val();
+    // const password = $(this).find('.password').val();
+    // gonna have password later?
+    $.ajax({
+      method: 'POST',
+      url: '/login',
+      data: {
+        handle: handle
+        // password: password
+      }
+    });
   });
 });
