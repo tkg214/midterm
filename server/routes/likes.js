9c68f802aee5a6e0fbf380463ca77b516c7a5402
vagrant <ecatevatis@gmail.com>
2017-03-03 17:56:58 +0000

    1 small bug left, breaking chain in async promises, will fix later

--- server/routes/likes.js
+++ server/routes/likes.js
@@ -3,32 +3,17 @@ const likesRoute  = require('express').Router();
 module.exports = function(fn) {
 
   likesRoute.post('/', (req, res, callback) => {
-    if(req.body.postid){
+    if(req.body.postid & req.body.like){
       let postID = req.body.postid;
       let userID = req.session.userID[0].id;
-      fn.getUsersLikes(postID, (users) => {
-        users.rows.forEach((id) => {
-          if (id.user_id === userID){
-            fn.delUserLikes(postID, userID, () => {
-              fn.getLikes(postID, (likes) => {
-                // res.send(likes);
-                callback();
-              });
-            });
-          }
-        });
-        // fn.incUserLikes(postID, userID, () => {
-        //   fn.getLikes(postID, (likes) => {
-        //     res.send(likes);
-        //     return;
-        //   });
-        // });
-        // return;
+      fn.incLikes(postID, userID, (likes) => {
+        res.send(likes);
       });
-    }
-    if(!req.body.userID) {
+
+    } else {
       fn.getLikes(req.body.postid, (likes) => {
         res.send(likes);
+        return;
       });
     }
 
