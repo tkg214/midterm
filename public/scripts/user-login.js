13889ef8d37283334bf64730408d492369495484
Hans Kurniadi <hans.d.k@live.com>
2017-03-03 02:16:37 +0000

    add login modal, ajax not yet implemented

--- public/scripts/user-login.js
+++ public/scripts/user-login.js
@@ -1,17 +1,23 @@
-$(document).ready(() => {
+$(document).ready(function() {
   //Selectors need to be changed
-  $('login-button').on('submit', () => {
-    const username = $('text-form-username').val();
-    const password = $('password-form').val();
-    $.ajax({
-      method: 'POST',
-      url: '/login',
-      data: {
-        username: username,
-        password: password
-      }
-    }).then(() => {
-      //createPost(results);
-    });
+  $('#login-form').on('submit', function(event) {
+    event.preventDefault();
+    const $this = $(this);
+    // console.log($this);
+    const username = $(this).find('.username').val();
+    const password = $(this).find('.password').val();
+    // console.log('username', username);
+    // console.log('password', password);
+    // alert('clicked');
+    // $.ajax({
+    //   method: 'POST',
+    //   url: '/login',
+    //   data: {
+    //     username: username,
+    //     password: password
+    //   }
+    // }).then(() => {
+    //   //createPost(results);
+    // });
   });
 });
