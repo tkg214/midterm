cb42ce7938f809196a34a5c0a819f828472d7ae6
Hans Kurniadi <hans.d.k@live.com>
2017-03-06 08:30:23 +0000

    get all post with more data still not working, commented out

--- server/routes/allposts.js
+++ server/routes/allposts.js
@@ -6,23 +6,50 @@ module.exports = function(fn) {
 
   allPostsRoute.get('/', (req, res) => {
     fn.getAllPosts((posts) => {
-      posts.forEach((post) => {
-        fn.getLikes(post.id, (likes) => {
-          post.num_likes = likes;
-        });
-        fn.getTag(post.id, (tag) => {
-          post.tag = tag;
-        });
-        fn.getComments(post.id, (comments) => {
-          post.comments = comments.rows;
-        });
-        fn.getRating(post.id, (rating) => {
-          post.rating = rating;
-        })
-        console.log('post data:', post);
-      });
       res.json(posts);
     });
+
+    // let allPosts = {};
+    // fn.getAllPosts((posts) => {
+    //   //console.log(posts);
+    //   let index = 0;
+    //   while(index < posts.length) {
+    //     let postId = posts[index].id;
+    //     console.log('post id', postId);
+    //     fn.getPostRelatedData(postId, (data) => {
+    //       let postData = data.rows[0];
+    //       allPosts[index+1] = postData;
+    //       console.log('new post', allPosts[index+1]);
+    //     });
+    //     index += 1;
+    //   }
+    // });
+    // console.log(allPosts);
+    // res.json(allPosts);
+
+
+
+    // fn.getAllPosts((posts) => {
+    //   posts.forEach((post) => {
+    //     fn.getLikes(post.id, (likes) => {
+    //       fn.getTag(post.id, (tag) => {
+    //         fn.getComments(post.id, (comments) => {
+    //           fn.getRating(post.id, (rating) => {
+    //             post.num_likes = likes;
+    //             post.tag = tag;
+    //             post.comments = comments.rows;
+    //             post.rating = rating;
+    //             //onsole.log('posts data inside:', post);
+    //             //console.log('posts data outside:', posts);
+    //           });
+    //         });
+    //       });
+    //     });
+    //     res.json(posts);
+    //   });
+    //   // console.log('posts data outside:', posts);
+    //   // res.json(posts);
+    // });
   });
 
   return allPostsRoute;
